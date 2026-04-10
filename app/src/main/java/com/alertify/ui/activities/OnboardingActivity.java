2b1b6b80269738af3ab2cecffab1b81b8abe24cd
prasadprakashh <prasadprakash2610@gmail.com>
2026-04-10 12:49:09 +0530

    i have done chnages as 4th member for ui development of app

---
+++ app/src/main/java/com/alertify/ui/activities/OnboardingActivity.java
@@ -0,0 +1,24 @@
+package com.alertify.ui.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import com.alertify.R;
+
+public class OnboardingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding);
+
+        Button btnNext = findViewById(R.id.btnNext);
+        btnNext.setOnClickListener(v -> {
+            // After onboarding, go to the Fake Calculator (Stealth Entry)
+            Intent intent = new Intent(OnboardingActivity.this, FakeCalculatorActivity.class);
+            startActivity(intent);
+            finish();
+        });
+    }
+}
